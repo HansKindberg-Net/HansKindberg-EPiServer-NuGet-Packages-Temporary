dec5a2ff6552a36d63552c06608e38fcbcd51337
Hans Kindberg <hans.kindberg@gmail.com>
2014-04-16 17:42:28 +0200

    Code backup

--- HansKindberg.EPiServer.NuGet.Packages.Tests.WebApplication/Properties/AssemblyInfo.cs
+++ HansKindberg.EPiServer.NuGet.Packages.Tests.WebApplication/Properties/AssemblyInfo.cs
@@ -2,19 +2,17 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Hans Kindberg - open source")]
-[assembly: AssemblyConfiguration(
-#if DEBUG
-"Debug"
-#else
-	"Release"
-#endif
-	)]
 [assembly: AssemblyDescription("A web-application for testing HansKindberg-EPiServer-NuGet-Packages.")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-0")]
-[assembly: AssemblyProduct("HansKindberg.EPiServer.NuGet.Packages.Tests.WebApplication 1.0.0-alpha-0")]
-[assembly: AssemblyTitle("HansKindberg.EPiServer.NuGet.Packages.Tests.WebApplication 1.0.0-alpha-0")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
-[assembly: ComVisible(false)]
 [assembly: Guid("e0089199-ff4f-4784-8b84-cb54fce19487")]
+
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.EPiServer.NuGet.Packages.Tests.WebApplication";
+
+	#endregion
+}
---
+++ Properties/Solution-AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Hans Kindberg - open source")]
+[assembly: AssemblyConfiguration(
+#if DEBUG
+	"Debug"
+#else
+	"Release"
+#endif
+	)]
+[assembly: AssemblyInformationalVersion("7.5.0-alpha-0")]
+[assembly: AssemblyProduct(AssemblyInfo.AssemblyName)]
+[assembly: AssemblyTitle(AssemblyInfo.AssemblyName)]
+[assembly: AssemblyVersion("7.5.0.0")]
+[assembly: ComVisible(false)]
